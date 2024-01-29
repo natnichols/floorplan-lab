b71a71e6cfd5e35559d6562e18742862182c68bc
NatNichols <natalie.e.nichols@gmail.com>
2024-01-29 16:17:56 -0500

    create Sink component

--- src/Kitchen.jsx
+++ src/Kitchen.jsx
@@ -1,4 +1,5 @@
 import Oven from "./Oven"
+import Sink from "./Sink"
 
 const Kitchen = (props) => {
   return ( 
@@ -6,6 +7,9 @@ const Kitchen = (props) => {
       <h1>Kitchen</h1>
         <div>
           <Oven />
+        </div>
+        <div>
+          <Sink />
         </div>
     </>
   )
